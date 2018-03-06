33c3629f1eb3aac1e8b34b4ed701bdb1dd772461
Colin Atkinson <colin.william.atkinson@gmail.com>
2018-03-06 17:47:54 -0500

    dammit tim.

--- __tests__/way_too_long.ts
+++
@@ -1,1 +0,0 @@
-interface ISetDict {    [key: string]: boolean;}export default class CustomSet {    private keys: ISetDict = {};    public add(item: string) {        this.keys[item] = true;       return this;    }    public has(item: string) {        return (item in this.keys);    }}
