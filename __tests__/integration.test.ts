9891353e4638f04edd9fd1a85fc3ed5cf9a2cb94
Colin Atkinson <colin.william.atkinson@gmail.com>
2018-04-10 16:30:57 -0400

    Add setup for integration testing

---
+++ __tests__/integration.test.ts
@@ -0,0 +1,23 @@
+import Client from 'bitcoin-core';
+
+describe('Network integration testing', async () => {
+    const client = new Client({
+        port: 12001,
+        username: 'user',
+        password: 'password',
+        version: '0.16.0',
+        host: '127.0.0.1',
+        network: 'regtest',
+    });
+
+    // const userWif = 'cSfYjfhSotRuhYx7n8ttCFdbHRZiMxwdmBPyJb2BZBous96sbSkh';
+    // const serviceWif = 'cMb11yoKHey8fp26zXV1vbeGCmw62nXJ8SVQJ6Uk2vdK6mEQyhNX';
+
+    beforeAll(async () => {
+        await client.generate(101);
+    });
+
+    it('Publishes a commit tx', async () => {
+        await client.getBlockCount();
+    });
+});
