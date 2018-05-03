b2fb8c2b743693c003dfd2e168ad7abec479e1ca
yongabyte <fmossaic@gmail.com>
2018-05-03 04:36:01 -0400

    changed to index.ts

--- lib/api.ts
+++
@@ -1,7 +0,0 @@
-export * from "./chain";
-export * from "./net";
-export * from "./errors";
-export * from "./utils";
-export * from "./verify";
-export * from "./txs";
-export { default } from "./TXList";
