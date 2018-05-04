67beeb6e3cf6368542e3fb63271526eafb0cf302
Jia Yong Zeng <yongabyte@users.noreply.github.com>
2018-05-03 21:56:32 -0400

    linting

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-export * from "./lib/chain";
-export * from "./lib/net";
-export * from "./lib/errors";
-export * from "./lib/utils";
-export * from "./lib/verify";
-export * from "./lib/txs";
-export { default } from "./lib/TXList";
+export * from './lib/chain';
+export * from './lib/net';
+export * from './lib/errors';
+export * from './lib/utils';
+export * from './lib/verify';
+export * from './lib/txs';
+export { default } from './lib/TXList';
